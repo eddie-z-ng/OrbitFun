8bd3e0cf40dd1d56b7a42cc227a98645066a2c48
Eddie Ng <wards.ng@gmail.com>
2014-01-25 18:02:57 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Prevent scrolling */
+
+if(window.addEventListener){
+    window.addEventListener('DOMMouseScroll',wheel,false);
+}
+
+function wheel(event)
+{
+    event.preventDefault();
+    event.returnValue=false;
+}
+window.onmousewheel=document.onmousewheel=wheel;
